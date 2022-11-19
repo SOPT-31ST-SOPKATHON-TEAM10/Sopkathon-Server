f6b072afab55e0394feeef1eb3352b6eae8d7c77
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-11-20 02:10:57 +0900

    Chore: 상수 추가

---
+++ src/constants/responseMessage.ts
@@ -0,0 +1,21 @@
+export default {
+
+    SUCCESS_CREATE_USER: "유저 생성 성공",
+    SUCCESS_CREATE_MESSAGE: "메세지 생성 성공",
+    SUCCESS_GET_MESSAGES: "메세지 목록 조회 성공",
+    SUCCESS_GET_MESSAGE_DETAIL: "메시지 상세 조회 성공",
+    SUCCESS_GET_CATEGORY: "카테고리별 메시지 개수 조회 성공",
+
+
+    NULL_VALUE: "필요한 값이 없습니다.",
+    ALREADY_NICKNAME: "이미 사용중인 닉네임입니다.",
+    NO_USER: "해당 아이디가 존재하지 않습니다",
+    NO_MESSAGE:  "해당 메시지가 존재하지 않습니다",
+
+    INTERNAL_SERVER_ERROR: "서버 내부 에러",
+
+
+    NOT_FOUND: "잘못된 경로입니다.",
+    BAD_REQUEST: "잘못된 요청입니다.",
+    
+};
