b44a68c41b626cf78c8cc93282049edca81bf30d
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-11-20 05:13:15 +0900

    Feat: 중복 닉네임 체크

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -5,11 +5,21 @@ import { userService } from "../service";
 
 const createUser = async (req: Request, res: Response) => {
     const { nickname } = req.body;
-    const data = await userService.createUser(nickname);
 
-    // const result = {
-        
-    // }
+    if (!nickname) {
+        return res.status(statusCode.BAD_REQUEST)
+        .send(fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    }
+
+    const user = await userService.findUserByNickname(nickname);
+
+    // 유저 중복 조회
+    if (user.length > 0) {
+        return res.status(statusCode.BAD_REQUEST)
+        .send(fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_NICKNAME))
+    }
+
+    const data = await userService.createUser(nickname);
 
     // 유저 생성 성공
     return res.status(statusCode.CREATED)
