7676efb99fd12ba15b8853f85a3ccd9e4a6e868c
unknown <dragontaek98@naver.com>
2022-11-20 05:39:55 +0900

    Feat: 카테고리 메세지 조회

--- src/controller/messageController.ts
+++ src/controller/messageController.ts
@@ -15,8 +15,22 @@ const createMessage = async (req: Request, res: Response) => {
     return res.status(statusCode.CREATED).send(success(statusCode.CREATED, responseMessage.SUCCESS_CREATE_MESSAGE, data));
 };
 
+const getCategoryMessage = async (req: Request, res: Response) => {
+    const { categoryId } = req.params;
+    const nickname = req.query.nickname as string;
+    const isOpened = req.query.isopened as string;
+
+    const data = await messageService.getCategoryMessage(Number(categoryId),nickname,Number(isOpened));
+
+    if (!data) {
+        return res.status(statusCode.BAD_REQUEST).send(fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+    }
+    return res.status(statusCode.CREATED).send(success(statusCode.CREATED, responseMessage.SUCCESS_CREATE_MESSAGE, data));
+};
+
 const messageController = {
-    createMessage
+    createMessage,
+    getCategoryMessage
 };
 
 export default messageController;
