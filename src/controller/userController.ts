5b7749063261d791e17a75a4f479c358ed13e6e7
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-11-20 02:58:20 +0900

    Feat: 유저 생성

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,5 +1,24 @@
-const userController = {
+import { Request, Response } from "express";
+import { responseMessage, statusCode } from "../constants";
+import { fail, success } from "../constants/response";
+import { userService } from "../service";
 
+const createUser = async (req: Request, res: Response) => {
+    const { nickname } = req.body;
+    const data = await userService.createUser(nickname);
+
+    // const result = {
+        
+    // }
+
+    // 유저 생성 성공
+    return res.status(statusCode.CREATED)
+         .send(success(statusCode.CREATED, responseMessage.SUCCESS_CREATE_USER, data));
+
+}
+
+const userController = {
+    createUser,
 };
 
 export default userController;
--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { userController } from '../controller';
 
 const router: Router = Router();
 
 // * 유저 생성 
-//router.post('/', userController.createUser);
+router.post('/', userController.createUser);
 
 export default router;
